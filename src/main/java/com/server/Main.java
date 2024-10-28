0f635c3587cdc0ba6d8b64bd3ef57165d52e3171
rigo72 <mattia.gori27@gmail.com>
2024-10-28 13:23:41 +0100

    Commit 2

--- src/main/java/com/server/Main.java
+++ src/main/java/com/server/Main.java
@@ -3,17 +3,26 @@ package com.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Main {
+    private static List<String> notes = new ArrayList<>();
     public static void main(String[] args) throws IOException{
         System.out.println("SERVER AVVIATO");
-
-        ServerSocket ss = new ServerSocket(8647);
-
-        do {
-            Socket socket = ss.accept();
-            MyThread t = new MyThread(socket);
-            t.start(); 
-        } while(true);
+        try(ServerSocket ss = new ServerSocket(3000)){
+            while (true) {
+                Socket clienSocket = ss.accept();
+                System.out.println("Nuovo Client Collegato");
+                new Thread(new MyThread(clienSocket)).start();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public static synchronized void addNote(String note){
+        notes.add(note);
+    }
+    public static synchronized List<String> getNotes(){
+        return notes;
     }
 }
--- src/main/java/com/server/MyThread.java
+++ src/main/java/com/server/MyThread.java
@@ -1,58 +1,45 @@
 package com.server;
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
-
+import java.util.List;
 public class MyThread extends Thread{
-
-    private static int thread_count = 0;
     private Socket s;
-
     public MyThread(Socket s) {
-        thread_count++;
-        System.out.println("Thread - " + thread_count + " - creato");
         this.s = s;
     }
-
+    @Override
     public void run() {    
-                     // Metodo run() contiene il codice asoncrono di questo thread
-        try {
-            BufferedReader in = new BufferedReader(new InputStreamReader(this.s.getInputStream()));
-            DataOutputStream out = new DataOutputStream(this.s.getOutputStream());
-            int received_choice;
-            do {
-                String received_string = in.readLine();
-                received_choice = Integer.parseInt(in.readLine());
-
-                System.out.println(received_string);
-                System.out.println(received_choice);
-                String editedString = editString(received_string, received_choice);
-                
-                out.writeBytes(editedString + '\n');
-                System.out.println("** AGGIORNATO E INVIATO LA STRINGA MODIFICATA**");
-            } while(received_choice >= 1 || received_choice <= 4);
-        } catch (IOException e) {
-            System.out.println("ERRORE NELLA COMUNICAZIONE");
+        try(
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            PrintWriter out = new PrintWriter(s.getOutputStream(), true)
+        ) {
+            String inputLine;
+            while ((inputLine=in.readLine())!=null) {
+                if (inputLine.equals("!")) {
+                    break;
+                }else if(inputLine.equals("?")){
+                    List<String> notes = Main.getNotes();
+                    for(String note : notes){
+                        out.println(note);
+                    }
+                    out.println("@");
+                }else{
+                    Main.addNote(inputLine);
+                    out.println("OK");
+                }
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally{
+            try{
+                s.close();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
         }
     }
-
-    private String editString(String s, int c) {
-        switch(c) {
-            case 1:
-                return s.toUpperCase();
-            case 2:
-                return s.toLowerCase();
-            case 3:
-                return new StringBuilder(s).reverse().toString();
-            case 4:
-                return String.valueOf(s.length());
-            default:
-                System.out.println("Scelta non valida");
-                return null;
-        }
-    }
-
 }
